3eaa97f940d9730a392179574a8360b1143ff121
carlofabyss <carlofabyss@hotmail.com>
2021-09-07 18:19:53 -0700

    First component

--- application/src/OdaiDashboard.js
+++ application/src/OdaiDashboard.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function OdaiDashboard() {
     return(
         <div>
-            Let The Buddies Hit The Floor Buddy
+            <h3>Let The Buddies Hit The Floor Buddy</h3>
         </div>
     )
 }
